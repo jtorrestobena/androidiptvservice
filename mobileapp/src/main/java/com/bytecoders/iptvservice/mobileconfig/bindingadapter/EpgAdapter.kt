f9ffb7dfa83e1fc0b2a417f1a534694155b32816
bytecoders <josepantoni.torres@gmail.com>
2020-02-23 15:58:40 +0100

    Set maxlines on EPG items and disable on tap, reset when the view is no longer visible

--- mobileapp/src/main/java/com/bytecoders/iptvservice/mobileconfig/bindingadapter/EpgAdapter.kt
+++ mobileapp/src/main/java/com/bytecoders/iptvservice/mobileconfig/bindingadapter/EpgAdapter.kt
@@ -20,10 +20,34 @@ class EpgAdapter(private val list: List<Program>): RecyclerView.Adapter<EpgAdapt
         holder.bind(list[position])
     }
 
+    override fun onViewDetachedFromWindow(holder: EpgViewHolder) {
+        super.onViewDetachedFromWindow(holder)
+        holder.close()
+    }
+
     class EpgViewHolder(private val binding: EpgItemBinding): RecyclerView.ViewHolder(binding.root) {
+        private var expanded = false
+        private val defaultLinesEpg by lazy {
+            itemView.context.resources.getInteger(R.integer.epg_description_max_lines)
+        }
+
         fun bind(program: Program) {
             binding.program = program
             binding.executePendingBindings()
+            itemView.setOnClickListener {
+                // Show any remaining lines on tap
+                if (!expanded) {
+                    binding.programDescription.maxLines = Int.MAX_VALUE
+                    expanded = true
+                }
+            }
+        }
+
+        fun close() {
+            if (expanded) {
+                binding.programDescription.maxLines = defaultLinesEpg
+                expanded = false
+            }
         }
     }
 }
