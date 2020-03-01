7ed1e98966d60935bc38ed5d7838db65df501d11
bytecoders <josepantoni.torres@gmail.com>
2020-03-01 19:55:34 +0100

    Simple event storage

--- mobileapp/src/androidTest/java/com/bytecoders/iptvservice/mobileconfig/ExampleInstrumentedTest.kt
+++ mobileapp/src/androidTest/java/com/bytecoders/iptvservice/mobileconfig/ExampleInstrumentedTest.kt
@@ -2,7 +2,12 @@ package com.bytecoders.iptvservice.mobileconfig
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
-import org.junit.Assert.assertEquals
+import com.bytecoders.iptvservice.mobileconfig.database.EventLog
+import com.bytecoders.iptvservice.mobileconfig.database.EventType
+import com.bytecoders.iptvservice.mobileconfig.database.getAppDatabase
+import org.junit.Assert
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
 import org.junit.Test
 import org.junit.runner.RunWith
 
@@ -14,9 +19,30 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
     @Test
-    fun useAppContext() {
-        // Context of the app under test.
+    fun databaseTest() {
+        val eventList = listOf(EventLog(EventType.type_error, "error title", "error message"))
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.bytecoders.iptvservice.mobileconfig", appContext.packageName)
+        val eventLogDatabase = getAppDatabase(appContext).eventLogDao()
+        eventLogDatabase.deleteAll()
+
+        assertTrue(eventLogDatabase.getAllEvents().isEmpty())
+        // add events
+        eventList.forEach {
+            eventLogDatabase.insertEvents(it)
+        }
+
+        // retrieve all
+        val storedEvents = eventLogDatabase.getAllEvents()
+        assertFalse(storedEvents.isEmpty())
+        // compare
+        Assert.assertArrayEquals(eventList.toTypedArray(), storedEvents.toTypedArray())
+
+        // delete
+        storedEvents.forEach {
+            eventLogDatabase.delete(it)
+        }
+
+        // empty
+        assertTrue(eventLogDatabase.getAllEvents().isEmpty())
     }
 }
---
+++ mobileapp/src/main/java/com/bytecoders/iptvservice/mobileconfig/database/EventLog.kt
@@ -0,0 +1,70 @@
+package com.bytecoders.iptvservice.mobileconfig.database
+
+import android.content.Context
+import androidx.room.*
+
+enum class EventType(val value: Int) {
+    type_debug (0),
+    type_information (1),
+    type_error (2)
+}
+
+class EventTypeConverter{
+
+    @TypeConverter
+    fun fromEventType(eventType: EventType): Int = eventType.value
+
+    @TypeConverter
+    fun toEventType(value: Int): EventType =
+        when(value) {
+            EventType.type_debug.value -> EventType.type_debug
+            EventType.type_information.value -> EventType.type_information
+            EventType.type_error.value -> EventType.type_error
+            else -> EventType.type_error
+        }
+}
+
+@Entity
+data class EventLog(
+        @ColumnInfo(name = "event_type") val eventType: EventType,
+        @ColumnInfo(name = "event_title") val eventTitle: String,
+        @ColumnInfo(name = "event_message") val eventMessage: String,
+        @ColumnInfo(name = "event_timestamp") val timestamp: Long = System.currentTimeMillis()
+) {
+    @PrimaryKey(autoGenerate = true) var id: Int = 0
+}
+
+@Dao
+interface EventLogDao {
+    @Query("SELECT * FROM EventLog")
+    fun getAllEvents(): List<EventLog>
+
+    @Query("SELECT * FROM EventLog WHERE event_timestamp BETWEEN :dateFrom AND :dateTo")
+    fun loadEventsForDate(dateFrom: Long, dateTo: Long): List<EventLog>
+
+    @Query("SELECT * FROM EventLog WHERE event_title LIKE :title")
+    fun findEventsByTitle(title: String): List<EventLog>
+
+    @Insert
+    fun insertEvents(vararg eventLog: EventLog)
+
+    @Delete
+    fun delete(eventLog: EventLog)
+
+    @Query("DELETE FROM EventLog")
+    fun deleteAll()
+}
+
+@Database(entities = [EventLog::class], version = 1)
+@TypeConverters(EventTypeConverter::class)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun eventLogDao(): EventLogDao
+}
+
+fun getAppDatabase(applicationContext: Context) = Room.databaseBuilder(
+                        applicationContext,
+                        AppDatabase::class.java, "eventlog-database"
+                        ).build()
+
+
+
