2aa9c82c77ee0b696fa54df7297d85c6d083325b
bytecoders <josepantoni.torres@gmail.com>
2020-11-22 19:19:03 +0100

    Check list download

--- mobileapp/src/androidTest/java/com/bytecoders/iptvservice/mobileconfig/HomeTest.kt
+++ mobileapp/src/androidTest/java/com/bytecoders/iptvservice/mobileconfig/HomeTest.kt
@@ -2,20 +2,25 @@ package com.bytecoders.iptvservice.mobileconfig
 
 
 import android.util.Log
+import androidx.recyclerview.widget.RecyclerView
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.contrib.RecyclerViewActions
 import androidx.test.espresso.matcher.RootMatchers.isDialog
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
 import com.bytecoders.iptvservice.mobileconfig.mockserver.MockIPTVServer
+import com.bytecoders.iptvservice.mobileconfig.mockserver.PROGRAMS_PER_CHANNEL
+import com.bytecoders.iptvservice.mobileconfig.util.Util.waitFor
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import kotlin.random.Random
 
 private const val TAG = "HomeTest"
 @LargeTest
@@ -27,6 +32,7 @@ class HomeTest {
     var mActivityTestRule = ActivityScenarioRule(MainActivity::class.java)
 
     private val mockIPTVServer = MockIPTVServer()
+    private val channelsToDownload by lazy { Random.nextInt(1, 100) }
 
     @Before
     fun setupHomeTest() {
@@ -45,21 +51,23 @@ class HomeTest {
 
     @Test
     fun downloadChannelList() {
-        val channelsToDownload = 20
-        performDownloadChannels(channelsToDownload)
+        performDownloadChannels()
         replyDownloadNewEPG(false)
     }
 
     @Test
     fun downloadChannelListAndEpg() {
-        val channelsToDownload = 20
-        performDownloadChannels(channelsToDownload)
+        performDownloadChannels()
         replyDownloadNewEPG(true)
         onView(withText("$channelsToDownload channels")).check(matches(isDisplayed()))
-        onView(withText("0 channel with EPG")).check(matches(isDisplayed()))
+        onView(waitFor(withText("$channelsToDownload channel with EPG"))).check(matches(isDisplayed()))
+        onView(waitFor(withText("${channelsToDownload*PROGRAMS_PER_CHANNEL} programs found"))).check(matches(isDisplayed()))
+        onView(withId(R.id.navigation_dashboard)).perform(click())
+        onView(withText(R.string.edit_off)).check(matches(isDisplayed()))
+        onView(withId(R.id.channelsRecyclerview)).perform(RecyclerViewActions.scrollTo<RecyclerView.ViewHolder>(hasDescendant(withText("Mock TV $channelsToDownload"))))
     }
 
-    private fun performDownloadChannels(channelsToDownload: Int) {
+    private fun performDownloadChannels() {
         onView(withId(R.id.iptv_url)).perform(clearText(), typeText(mockIPTVServer.getChannelsUrl(channelsToDownload)), closeSoftKeyboard())
         onView(withId(R.id.download_list)).perform(click())
     }
--- mobileapp/src/androidTest/java/com/bytecoders/iptvservice/mobileconfig/mockserver/MockIPTVServer.kt
+++ mobileapp/src/androidTest/java/com/bytecoders/iptvservice/mobileconfig/mockserver/MockIPTVServer.kt
@@ -1,10 +1,13 @@
 package com.bytecoders.iptvservice.mobileconfig.mockserver
 
+import android.icu.text.SimpleDateFormat
 import android.util.Log
 import okhttp3.mockwebserver.Dispatcher
 import okhttp3.mockwebserver.MockResponse
 import okhttp3.mockwebserver.MockWebServer
 import okhttp3.mockwebserver.RecordedRequest
+import java.util.*
+import java.util.concurrent.TimeUnit
 
 private const val PORT = 8081
 private const val CONNECTION_URL = "http://127.0.0.1:$PORT"
@@ -12,10 +15,12 @@ private const val CONNECTION_URL = "http://127.0.0.1:$PORT"
 private const val VERSION = 1.0
 private const val TAG = "MockIPTVServer"
 private const val M3U_HEADER = "#EXTM3U $TAG v$VERSION\n"
-private const val EPG_URL = "#EXTM3U url-tvg=\"${CONNECTION_URL}/epg\"\n"
+const val PROGRAMS_PER_CHANNEL = 20
 
 class MockIPTVServer : Dispatcher() {
     private val server by lazy { MockWebServer() }
+    private val dateFormat by lazy {
+        SimpleDateFormat("yyyyMMddHHmm00 +0100") }
 
     fun start() {
         server.dispatcher = this
@@ -32,6 +37,7 @@ class MockIPTVServer : Dispatcher() {
         path?.removeFirst()
         return when (path?.getOrNull(0)) {
             "channels" -> getChannelsResponse(path[1].toInt())
+            "epg" -> getEpgResponse(path[1].toInt())
             else -> MockResponse().setResponseCode(404)
         }
     }
@@ -39,12 +45,52 @@ class MockIPTVServer : Dispatcher() {
     fun getChannelsUrl(channelQuantity: Int): String = "$CONNECTION_URL/channels/$channelQuantity"
 
     private fun getChannelsResponse(numChannels: Int): MockResponse {
-        Log.d(TAG, "Creating $numChannels")
-        var channelBody = M3U_HEADER + EPG_URL
+        Log.d(TAG, "Creating $numChannels channels")
+        var channelBody = M3U_HEADER + getEpgUrl(numChannels)
         for (i in 0 until numChannels) {
-            channelBody +=  "#EXTINF:-1 tvg-id=\"MockChannel$i\" tvg-logo=\"https://mockChannelLogo$i\" group-title=\"Group_channel_$i\" tvg-name=\"Mock TV $i\",Mock TV $i\n" +
+            channelBody +=  "#EXTINF:-1 tvg-id=\"MockChannel$i\" tvg-logo=\"https://mockChannelLogo$i\" group-title=\"Group_channel_$i\" tvg-name=\"Mock TV ${i + 1}\",Mock TV $i\n" +
                     "https://supertvsource_channel$i"
         }
+        return MockResponse().setBody(channelBody).setResponseCode(200)
+    }
+
+    private fun getEpgUrl(numChannels: Int) = "#EXTM3U url-tvg=\"${CONNECTION_URL}/epg/$numChannels\"\n"
+
+    private fun getEpgResponse(numChannels: Int): MockResponse {
+        Log.d(TAG, "Creating EPG for $numChannels channels")
+        var channelBody = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+                "<tv generator-info-name=\"$TAG v$VERSION\">"
+        for (i in 0 until numChannels) {
+            channelBody += "<channel id=\"MockChannel$i\">\n" +
+                    "    <display-name lang=\"es\">Mock TV ${i + 1}</display-name>\n" +
+                    "    <icon src=\"https://mockChannelProgramLogo$i\" />\n" +
+                    "  </channel>\n"
+
+            val calendar = Calendar.getInstance()
+            calendar.time = Date()
+            for (p in 0 until PROGRAMS_PER_CHANNEL) {
+                channelBody += "<programme start=\"${dateFormat.format(Date(System.currentTimeMillis() + TimeUnit.HOURS.toMillis(p.toLong())))}\" stop=\"${dateFormat.format(Date(System.currentTimeMillis() + TimeUnit.HOURS.toMillis(p.toLong() + 1)))}\" channel=\"MockChannel$i\">\n" +
+                        "    <title lang=\"en\">Great Title</title>\n" +
+                        "    <desc lang=\"es\">Great description</desc>\n" +
+                        "    <credits>\n" +
+                        "      <presenter>Presenter 1</presenter>\n" +
+                        "      <presenter>Presenter 2</presenter>\n" +
+                        "    </credits>\n" +
+                        "    <date>${calendar.get(Calendar.YEAR)}</date>\n" +
+                        "    <category lang=\"en\">Entertainment</category>\n" +
+                        "    <category lang=\"en\">Humor</category>\n" +
+                        "    <icon src=\"https://www.programiconsource.com\" />\n" +
+                        "    <country lang=\"en\">Spain</country>\n" +
+                        "    <episode-num system=\"onscreen\">T15 E48</episode-num>\n" +
+                        "    <rating system=\"ES\">\n" +
+                        "      <value>12</value>\n" +
+                        "    </rating>\n" +
+                        "  </programme>\n"
+            }
+        }
+
+        channelBody += "</tv>"
+
         return MockResponse().setBody(channelBody).setResponseCode(200)
     }
 }
